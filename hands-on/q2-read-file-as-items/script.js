f8bc976f0d5298b0f5590053fedbfae7dabe9c93
Xu Jie Sng <sngxujie@gmail.com>
2023-10-24 04:16:47 +0000

    complete q2 part 2

--- hands-on/q2-read-file-as-items/script.js
+++ hands-on/q2-read-file-as-items/script.js
@@ -3,12 +3,25 @@ document.addEventListener("DOMContentLoaded", async function(){
 })
 
 // When the button is clicked run this function
+
 document.querySelector("#item-btn").addEventListener("click", function(){
+    // Hide the button to prevent multiple clicks
+    document.querySelector("#item-btn").style.display = "none"
+    // Run the loadData function
     return loadData()
 })
 
 // function that will load data when button is clicked
 async function loadData() {
-    let response = await axios.get("items.txt")
-    console.log(response.data)
+    const response = await axios.get("items.txt")
+    const fruits = response.data.split(",")
+    const fruitList = document.querySelector("#fruitList")
+    // for loop through the fruits
+    for (f of fruits) {
+        let li = document.createElement("li")
+        li.innerHTML = `${f}`
+    fruitList.appendChild(li)        
+    }
+    
+    // console.log(response.data) // returns "apple,oranges,pineapples,pears,durians"
 }
