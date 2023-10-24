ed235e1c96d3fb60429878f02024752f56fc7b38
Xu Jie Sng <sngxujie@gmail.com>
2023-10-24 07:32:15 +0000

    seperate title and image element and hide button after click

--- hands-on/q4-simple-json-example/script.js
+++ hands-on/q4-simple-json-example/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const loadImage = document.querySelector("#load-image-btn")
     loadImage.addEventListener("click", async function(){
         await loadArtwork()
+        return loadImage.style.display = "none"
     })    
     
 })
@@ -10,11 +11,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
 async function loadArtwork(){
     const response = await axios.get("artwork.json")
     const artwork = response.data
-    let display = document.createElement("div")
-    display.innerHTML = `<h1>${artwork.title}</h1>
-    <img src='${artwork.image_url}'/>`
-    display.classList.add = (`${artwork.class}`)
-    document.querySelector("#artwork").appendChild(display)
+    const title = document.createElement("h1")
+    const image = document.createElement("img") 
+    //change to seperate appendChild
+    title.innerHTML = `${artwork.title}`
+    image.style.width = "300px"
+    image.src = `${artwork.image_url}`
+    image.classList.add(`${artwork.class}`)
+    document.querySelector("#artwork").appendChild(title)
+    document.querySelector("#artwork").appendChild(image)
 
     //console.log(artwork)
 }
