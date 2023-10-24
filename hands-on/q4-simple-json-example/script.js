07b71388caa3fb7ad61c082e81adc6e19a3f6c86
Xu Jie Sng <sngxujie@gmail.com>
2023-10-24 07:06:12 +0000

    complete q4

--- hands-on/q4-simple-json-example/script.js
+++ hands-on/q4-simple-json-example/script.js
@@ -1,8 +1,20 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    loadArtwork()
+document.addEventListener("DOMContentLoaded", ()=>{
+   
+    const loadImage = document.querySelector("#load-image-btn")
+    loadImage.addEventListener("click", async function(){
+        await loadArtwork()
+    })    
+    
 })
 
 async function loadArtwork(){
-    let artwork = await axios.get("artwork.json")
-    console.log(artwork)
+    const response = await axios.get("artwork.json")
+    const artwork = response.data
+    let display = document.createElement("div")
+    display.innerHTML = `<h1>${artwork.title}</h1>
+    <img src='${artwork.image_url}'/>`
+    display.classList.add = (`${artwork.class}`)
+    document.querySelector("#artwork").appendChild(display)
+
+    //console.log(artwork)
 }
